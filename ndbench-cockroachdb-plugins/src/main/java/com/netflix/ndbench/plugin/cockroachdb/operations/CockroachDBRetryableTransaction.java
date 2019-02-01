b73cefc65e52db90b7a3a16e40945259fe994ae6
nchandrappa <nchandrappa@pivotal.io>
2019-02-01 09:30:05 -0500

    updates to ndbench project to include only geode and pcc

--- ndbench-cockroachdb-plugins/src/main/java/com/netflix/ndbench/plugin/cockroachdb/operations/CockroachDBRetryableTransaction.java
+++
@@ -1,9 +0,0 @@
-package com.netflix.ndbench.plugin.cockroachdb.operations;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface CockroachDBRetryableTransaction {
-    void run(Connection conn)
-            throws SQLException;
-}
